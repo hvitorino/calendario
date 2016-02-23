bb5bf3374323e623210242970dff3188e1887c77
Hamon Vitorino <hamon.vitorino@gmail.com>
2016-02-22 22:22:48 -0300

    Dojo 1 - 22-02-2016

--- src/dojo/CalendarioDaMaria.java
+++ src/dojo/CalendarioDaMaria.java
@@ -1,5 +1,6 @@
 package dojo;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class CalendarioDaMaria {
@@ -7,5 +8,30 @@ public class CalendarioDaMaria {
 	public boolean testarData(Date date) {
 		return false;
 	}
+
+	public boolean isTercaFeira(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+
+		if (calendar.get(Calendar.DAY_OF_WEEK) == calendar.TUESDAY) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	public boolean isQuintaFeira(Date date){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+
+		if (calendar.get(Calendar.DAY_OF_WEEK) == calendar.THURSDAY) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	
+	
 
 }
--- test/dojo/test/DadoUmCalendarioDaMaria.java
+++ test/dojo/test/DadoUmCalendarioDaMaria.java
@@ -2,8 +2,12 @@ package dojo.test;
 
 import static org.junit.Assert.assertTrue;
 
+import static org.junit.Assert.assertFalse;
+
+import java.util.Calendar;
 import java.util.Date;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import dojo.CalendarioDaMaria;
@@ -12,11 +16,59 @@ public class DadoUmCalendarioDaMaria {
 
 	private CalendarioDaMaria calendario;
 	
-	@Test
-	public void possoTestarSeUmaDataEhDiaDaMaria() {
-		calendario = new CalendarioDaMaria(); 
-		
-		assertTrue(calendario.testarData(new Date()));;
+	@Before
+	public void setUp() {
+		calendario = new CalendarioDaMaria();
 	}
 
+	@Test
+	public void confirmaTercaFeira() {
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, 23);
+		cal.set(Calendar.MONTH, 1);
+		cal.set(Calendar.YEAR, 2016);
+
+		Date date = cal.getTime();
+
+		assertTrue(calendario.isTercaFeira(date));
+	}
+
+	@Test
+	public void confirmaDiaDiferenteTercaFeira() {
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, 22);
+		cal.set(Calendar.MONTH, 1);
+		cal.set(Calendar.YEAR, 2016);
+
+		Date date = cal.getTime();
+
+		assertFalse(calendario.isTercaFeira(date));
+	}
+
+	@Test
+	public void devoVerificarSeEhQuintaFeira() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, 25);
+		cal.set(Calendar.MONTH, 1);
+		cal.set(Calendar.YEAR, 2016);
+
+		Date date = cal.getTime();
+		
+		assertTrue(calendario.isQuintaFeira(date));
+	}
+	
+	@Test
+	public void devoVerificarSeNaoEhQuintaFeira() {
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, 26);
+		cal.set(Calendar.MONTH, 1);
+		cal.set(Calendar.YEAR, 2016);
+
+		Date date = cal.getTime();
+		
+		assertFalse(calendario.isQuintaFeira(date));
+	}
 }
